a8e0726420e0bf526eca7f0a811a342ced95beab
Andrew C. Greenberg <wizardwerdna@gmail.com>
2016-06-11 13:30:59 -0700

    fix scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
-module.exports = {  
-  entry: './app.ts',
-  output: {
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
-  },
-  module: {
-    loaders: [
-      {
-        test   : /\.woff|\.woff2|\.svg|.eot|\.ttf/,
-        loader : 'url?prefix=font/&limit=10000'
-      },
-      {
-        test: /\.(jpg|png)$/,
-        loader: 'url?limit=100000'
-      },
-      { test: /\.ts$/, loader: 'ts-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' }
-    ]
-  }
+'use strict';
+var path = require('path');
+var resolve = path.resolve;
+module.exports = env => {
+  return {
+    entry: './app.ts',
+    output: {
+      filename: 'bundle.js',
+      path: resolve(__dirname, 'dist'),
+      pathinfo: !env.prod,
+    },
+    context: resolve(__dirname, 'src'),
+    resolve: {
+     extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    },
+    devtool: env.prod ? 'source-map' : 'eval',
+    bail: env.prod,
+    module: {
+      loaders: [
+        {
+          test   : /\.woff|\.woff2|\.svg|.eot|\.ttf/,
+          loader : 'url?prefix=font/&limit=10000'
+        },
+        {
+          test: /\.(jpg|png)$/,
+          loader: 'url?limit=100000'
+        },
+        { test: /\.ts$/, loader: 'ts-loader' },
+        { test: /\.css$/, loader: 'style-loader!css-loader' }
+      ],
+    },
+  };
 };
